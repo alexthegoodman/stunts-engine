dd06b9254aea0cdcc505a828c98254c5fb2e4201
Alex Goodman <alexthegoodman@gmail.com>
2024-12-14 21:50:29 -0500

    proper positioning

--- src/editor.rs
+++ src/editor.rs
@@ -840,7 +840,13 @@ impl Editor {
         if let Some(poly_index) = self.dragging_polygon {
             if let Some(on_mouse_up_creator) = &self.on_mouse_up {
                 let mut on_up = on_mouse_up_creator().expect("Couldn't get on handler");
-                let selected_sequence_data = on_up(poly_index, self.last_top_left);
+                let selected_sequence_data = on_up(
+                    poly_index,
+                    Point {
+                        x: self.last_top_left.x - 600.0,
+                        y: self.last_top_left.y - 50.0,
+                    },
+                );
                 self.update_motion_paths(&selected_sequence_data);
                 println!("Motion Paths updated!");
             }
